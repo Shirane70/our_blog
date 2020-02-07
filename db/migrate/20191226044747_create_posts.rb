10ce72294daa7ff55f47534cc47212b3e1993b01
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-02-07 13:44:04 +0900

    Update 20191226044747_create_posts.rb

--- db/migrate/20191226044747_create_posts.rb
+++ db/migrate/20191226044747_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
-    create_table :posts do |t|
-      t.text :title
+    create_table :posts, options: 'ROW_FORMAT=DYNAMIC' do |t|
+      t.references :category
+      t.text :title, null: false
       t.text :content
       t.timestamps
     end
