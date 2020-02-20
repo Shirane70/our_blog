001635965a9fcb9978675b27cbde541e4028b0be
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-02-20 20:23:30 +0900

    category-in-posttable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_081011) do
+ActiveRecord::Schema.define(version: 2020_02_20_104805) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "slug", null: false
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
@@ -18,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_081011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "category"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
